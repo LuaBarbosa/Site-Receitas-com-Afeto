cf1c4b61362f3cf85b8223809880cdf8b1c8c690
LuaBarbosa <lua.barbosa@yahoo.com.br>
2021-09-13 21:23:09 -0300

    Feat: Inclução de função para validar recados.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document
   .getElementById("botaoEnviar")
   .addEventListener("click", validaFormulario);
 
+document.getElementById("enviarRecado").addEventListener("click", validaRecado);
+
 function validaFormulario() {
   if (
     document.getElementById("nome").value != "" &&
@@ -11,5 +13,13 @@ function validaFormulario() {
     alert("Prontinho! Você receberá as novidades por email");
   } else {
     alert("Por favor, preencha os campos nome, email e telefone!");
+  }
+}
+
+function validaRecado() {
+  if (document.getElementById("textarea").value != "") {
+    alert("Agradecemos seu contato!");
+  } else {
+    alert("Por favor, preencha o campo de texto para deixar seu recado!");
   }
 }
